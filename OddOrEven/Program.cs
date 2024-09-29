1ed526233efd493a77910205da82d51d0c2e69c9
Gian Kyle Sombillo <gksombillo@csmbataan.ph.education>
2024-09-29 18:13:27 +0800

    Activity 4

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Step 1: Initialize test data
+        int num = 15;
+
+        // Step 2: Check if the number is even or odd
+        if (num % 2 == 0)
+        {
+            // Step 3: Print result for even number
+            Console.WriteLine($"{num} is an even integer");
+        }
+        else
+        {
+            // Step 4: Print result for odd number
+            Console.WriteLine($"{num} is an odd integer");
+        }
+    }
+}
